2a215e70036249504c8bebc04812e8f8e673ba4d
vinodhconnects <vinodhconnects@gmail.com>
2023-06-01 12:14:30 +0530

    new-one

--- script.js
+++ script.js
@@ -2,7 +2,14 @@ async function quoteRead()
 {
     let response = await fetch("https://api.quotable.io/random")
     let data = await response.text()
-    console.log(JSON.parse(data))
+    //console.log(JSON.parse(data))
+    quote =  JSON.parse(data)
+    author = quote.author
+    content = quote.content
+    let code = ""
+    code +="<p>"+ content + "</p>"
+    code +="<b><i> - "+author+"</i></b>"
+    document.getElementById("quotes-for-the-day").innerHTML=code
 }
 
 quoteRead()
